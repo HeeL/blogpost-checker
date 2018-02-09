2c19ad3da6fc99dbbb3b8afa8288d42b73651c2b
HeeL <parizhskiy@gmail.com>
2018-02-09 22:43:14 +0100

    Expose /_health route with 200 response for healthcheck

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const pullRequest = require('./src/pullRequest');
+const healthcheckPing = require('healthcheck-ping');
 
 module.exports = (robot) => {
   robot.log('Yay, the app was loaded!');
+
+  const app = robot.route();
+  app.use(healthcheckPing('_health'));
+
   robot.on('pull_request', pullRequest.bind(null, robot));
 };
--- test/unit/indexSpec.js
+++ test/unit/indexSpec.js
@@ -1,17 +1,37 @@
+jest.mock('healthcheck-ping');
+
 const index = require('../../index');
+const healthcheckPingMock = require('healthcheck-ping');
+
+function createRobotObject(routeStub) {
+  const route = routeStub || (() => ({ use: jest.fn() }));
+  return { log: jest.fn(), on: jest.fn(), route };
+}
 
 it('logs out robot success message on start', () => {
-  const robot = { log: jest.fn(), on: jest.fn() };
+  const robot = createRobotObject();
   index(robot);
 
   expect(robot.log).toHaveBeenCalledTimes(1);
 });
 
 it('handles pull request', () => {
-  const robot = { log: jest.fn(), on: jest.fn() };
+  const robot = createRobotObject();
   index(robot);
 
   expect(robot.on).toHaveBeenCalledTimes(1);
   const firstArgumentOfFunctionCall = robot.on.mock.calls[0][0];
   expect(firstArgumentOfFunctionCall).toEqual('pull_request');
+});
+
+it('uses healthcheck-ping middleware for exposing healthcheck route', () => {
+  const useStub = jest.fn();
+  const routeStub = jest.fn().mockReturnValue({ use: useStub });
+  const robot = createRobotObject(routeStub);
+  healthcheckPingMock.mockImplementation(() => 'foo-bar');
+  index(robot);
+
+  expect(robot.route).toHaveBeenCalledTimes(1);
+  expect(healthcheckPingMock).toHaveBeenCalledWith('_health');
+  expect(useStub).toHaveBeenCalledWith('foo-bar');
 });
