7f793853973bcd2dcf864b139c588fc27aabc823
HeeL <parizhskiy@gmail.com>
2018-02-10 01:26:23 +0100

    Disable comma-dangle rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "globals": {
-        "it": true,
-        "describe": true,
-        "jest": true,
-        "expect": true
-    }
+  "extends": "airbnb-base",
+  "rules": {
+    "comma-dangle": 0,
+  },
+  "globals": {
+    "it": true,
+    "describe": true,
+    "jest": true,
+    "expect": true
+  }
 };
