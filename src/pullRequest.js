e449712daf6c408e0244484f2dd0142a607f9d66
HeeL <parizhskiy@gmail.com>
2018-02-12 22:40:52 +0100

    Fix format of the fetcher response

--- src/pullRequest.js
+++ src/pullRequest.js
@@ -1,10 +1,12 @@
 const axios = require('axios');
+const R = require('ramda');
 const setPullRequestState = require('./setPullRequestState');
 const isPublishDateValid = require('./isPublishDateValid');
 
 module.exports = async (robot, context) => {
   if (context.payload.action === 'synchronize') {
     axios.get(context.payload.pull_request.diff_url)
+      .then(R.prop('data'))
       .then(isPublishDateValid)
       .then(setPullRequestState.bind(null, context));
   }
