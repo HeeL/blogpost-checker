de7491a18ccbf65baf21eeb13086f59f47c8948d
HeeL <parizhskiy@gmail.com>
2018-02-12 17:17:13 +0100

    Add check for publish date in post meta information

--- src/isPublishDateValid.js
+++ src/isPublishDateValid.js
@@ -9,9 +9,9 @@ const isOlderThanTwoDays = dateString =>
   diffDatesInDays(new Date(), new Date(dateString)) > 2;
 
 const containOutdatedPublishDate = (diffChunk) => {
-  const regexpParsePostDate = /-{3} \/dev\/null\n\s+\+{3} b\/_posts\/(\d{4}-\d{2}-\d{2})[a-z0-9-_]+\.md/gi;
-  const postDateMatches = regexpParsePostDate.exec(diffChunk);
-  if (postDateMatches && isOlderThanTwoDays(postDateMatches[1])) {
+  const regexpParsePostDate = /\n\s+\+date: (\d{4}-\d{2}-\d{2})/gi;
+  const publishDateMatches = regexpParsePostDate.exec(diffChunk);
+  if (publishDateMatches && isOlderThanTwoDays(publishDateMatches[1])) {
     return true;
   }
   return false;
