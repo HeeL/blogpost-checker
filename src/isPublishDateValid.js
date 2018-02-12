850c0b0ac5c2e01e7493707b7c81f53ac83cf2c4
HeeL <parizhskiy@gmail.com>
2018-02-12 13:00:22 +0100

    Refactor code that compares current date with provided date

--- src/isPublishDateValid.js
+++ src/isPublishDateValid.js
@@ -1,12 +1,10 @@
 const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
 
-const diffDatesInDays = (date) => {
-  const currentDate = new Date();
+const diffDatesInDays = (date1, date2) =>
+  Math.round(Math.abs((date1.getTime() - date2.getTime()) / (oneDayInMilliseconds)));
 
-  return Math.round(Math.abs((currentDate.getTime() - date.getTime()) / (oneDayInMilliseconds)));
-};
-
-const isOlderThanTwoDays = dateString => diffDatesInDays(new Date(dateString)) > 2;
+const isOlderThanTwoDays = dateString =>
+  diffDatesInDays(new Date(), new Date(dateString)) > 2;
 
 module.exports = (diffContent) => {
   const regexpParsePostDate = /-{3} \/dev\/null\n\s+\+{3} b\/_posts\/(\d{4}-\d{2}-\d{2})[a-z0-9-_]+\.md/gi;
