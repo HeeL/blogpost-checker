a6a27a4db12e218157632625c6cf88dbc9010674
HeeL <parizhskiy@gmail.com>
2018-02-12 16:36:32 +0100

    Perform checks in diff chunks instead of the whole diff

--- src/isPublishDateValid.js
+++ src/isPublishDateValid.js
@@ -6,11 +6,17 @@ const diffDatesInDays = (date1, date2) =>
 const isOlderThanTwoDays = dateString =>
   diffDatesInDays(new Date(), new Date(dateString)) > 2;
 
-module.exports = (diffContent) => {
+const containOutdatedPublishDate = (diffChunk) => {
   const regexpParsePostDate = /-{3} \/dev\/null\n\s+\+{3} b\/_posts\/(\d{4}-\d{2}-\d{2})[a-z0-9-_]+\.md/gi;
-  const postDateMatches = regexpParsePostDate.exec(diffContent);
+  const postDateMatches = regexpParsePostDate.exec(diffChunk);
   if (postDateMatches && isOlderThanTwoDays(postDateMatches[1])) {
-    return false;
+    return true;
   }
-  return true;
+  return false;
+};
+
+module.exports = (diffContent) => {
+  const diffChunks = diffContent.split('  diff --git');
+
+  return (diffChunks.filter(containOutdatedPublishDate)).length === 0;
 };
