416de1bf3c672cbaa0103183d101521101f0bb2e
HeeL <parizhskiy@gmail.com>
2018-02-12 17:41:25 +0100

    Add extra check for new files that are not blogposts

--- test/unit/src/isPublishDateValidSpec.js
+++ test/unit/src/isPublishDateValidSpec.js
@@ -18,10 +18,17 @@ describe('isPublishDateValid', () => {
     expect(isPublishDateValid(gitDiffNewFileWithOldDate)).toBe(false);
   });
 
-  it('returns true for diff with date older than 2 days if its not a new file', () => {
+  it('returns true for diff with date older than 2 days if its NOT a new file', () => {
     MockDate.set('2018-01-15');
     const gitDiffEditFileWithOldDate = fs.readFileSync('./test/unit/resources/gitDiffEditFileWithOldDate.txt', 'utf8');
 
     expect(isPublishDateValid(gitDiffEditFileWithOldDate)).toBe(true);
+  });
+
+  it('returns true for new files diff if its not a blogpost', () => {
+    MockDate.set('2018-01-15');
+    const gitDiffNewFileWithOldDateButNotBlogpost = fs.readFileSync('./test/unit/resources/gitDiffNewFileWithOldDateButNotBlogpost.txt', 'utf8');
+
+    expect(isPublishDateValid(gitDiffNewFileWithOldDateButNotBlogpost)).toBe(true);
   });
 });
