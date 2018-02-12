5dfb12df8e4cc98cc7b00374963fba838377da78
HeeL <parizhskiy@gmail.com>
2018-02-12 12:59:06 +0100

    Add current date mock with mockdate package

--- test/unit/src/isPublishDateValidSpec.js
+++ test/unit/src/isPublishDateValidSpec.js
@@ -1,12 +1,18 @@
 const fs = require('fs');
+const MockDate = require('mockdate');
 const isPublishDateValid = require('../../../src/isPublishDateValid');
 
 describe('isPublishDateValid', () => {
+  afterEach(() => {
+    MockDate.reset();
+  });
+
   it('returns true for empty string', () => {
     expect(isPublishDateValid('')).toBe(true);
   });
 
   it('returns false for diff with date older than 2 days', () => {
+    MockDate.set('2018-01-15');
     const gitDiffNewFileWithOldDate = fs.readFileSync('./test/unit/resources/gitDiffNewFileWithOldDate.txt', 'utf8');
 
     expect(isPublishDateValid(gitDiffNewFileWithOldDate)).toBe(false);
