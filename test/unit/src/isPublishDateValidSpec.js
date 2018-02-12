4c7bffdddea8b705dd267e47ab84faf7f5fa0a99
HeeL <parizhskiy@gmail.com>
2018-02-12 18:03:50 +0100

    Add some more use cases
    - approve diff with date from the future
    - approve diff with date that is 1 day old
    - approve diff with date that is 2 days old

--- test/unit/src/isPublishDateValidSpec.js
+++ test/unit/src/isPublishDateValidSpec.js
@@ -18,6 +18,27 @@ describe('isPublishDateValid', () => {
     expect(isPublishDateValid(gitDiffNewFileWithOldDate)).toBe(false);
   });
 
+  it('returns true for diff with date that is 1 day old', () => {
+    MockDate.set('2018-01-13');
+    const gitDiffNewFileWithOldDate = fs.readFileSync('./test/unit/resources/gitDiffNewFileWithOldDate.txt', 'utf8');
+
+    expect(isPublishDateValid(gitDiffNewFileWithOldDate)).toBe(true);
+  });
+
+  it('returns true for diff with date that is 2 days old', () => {
+    MockDate.set('2018-01-14');
+    const gitDiffNewFileWithOldDate = fs.readFileSync('./test/unit/resources/gitDiffNewFileWithOldDate.txt', 'utf8');
+
+    expect(isPublishDateValid(gitDiffNewFileWithOldDate)).toBe(true);
+  });
+
+  it('returns true for diff with date from the future', () => {
+    MockDate.set('2018-01-11');
+    const gitDiffNewFileWithOldDate = fs.readFileSync('./test/unit/resources/gitDiffNewFileWithOldDate.txt', 'utf8');
+
+    expect(isPublishDateValid(gitDiffNewFileWithOldDate)).toBe(true);
+  });
+
   it('returns true for diff with date older than 2 days if its NOT a new file', () => {
     MockDate.set('2018-01-15');
     const gitDiffEditFileWithOldDate = fs.readFileSync('./test/unit/resources/gitDiffEditFileWithOldDate.txt', 'utf8');
