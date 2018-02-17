afcf385a60362409e5181e81ba2f7d8a9b9557d5
HeeL <parizhskiy@gmail.com>
2018-02-17 21:00:15 +0100

    Remove healthcheck because probot includes /ping route

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 const pullRequest = require('./src/pullRequest');
-const healthcheckPing = require('healthcheck-ping');
 
-module.exports = (robot) => {
-  robot.log('Yay, the app was loaded!');
-
-  const app = robot.route();
-  app.use(healthcheckPing('_health'));
-
-  robot.on(['pull_request.opened', 'pull_request.synchronize'], pullRequest);
-};
+module.exports = robot => robot.on(['pull_request.opened', 'pull_request.synchronize'], pullRequest);
--- test/unit/indexSpec.js
+++ test/unit/indexSpec.js
@@ -1,38 +1,11 @@
-jest.mock('healthcheck-ping');
-
 const index = require('../../index');
-const healthcheckPingMock = require('healthcheck-ping');
+const pullRequestHandler = require('../../src/pullRequest');
 
-function createRobotObject(routeStub) {
-  const route = routeStub || (() => ({ use: jest.fn() }));
-  return { log: jest.fn(), on: jest.fn(), route };
-}
-
-it('logs out robot success message on start', () => {
-  const robot = createRobotObject();
-  index(robot);
-
-  expect(robot.log).toHaveBeenCalledTimes(1);
-});
-
-it('handles pull request', () => {
+it('handles pull request open and synchronize', () => {
   const expectedListeners = ['pull_request.opened', 'pull_request.synchronize'];
-  const robot = createRobotObject();
+  const robot = { on: jest.fn() };
   index(robot);
 
   expect(robot.on).toHaveBeenCalledTimes(1);
-  const firstArgumentOfFunctionCall = robot.on.mock.calls[0][0];
-  expect(firstArgumentOfFunctionCall).toEqual(expectedListeners);
-});
-
-it('uses healthcheck-ping middleware for exposing healthcheck route', () => {
-  const useStub = jest.fn();
-  const routeStub = jest.fn().mockReturnValue({ use: useStub });
-  const robot = createRobotObject(routeStub);
-  healthcheckPingMock.mockImplementation(() => 'foo-bar');
-  index(robot);
-
-  expect(robot.route).toHaveBeenCalledTimes(1);
-  expect(healthcheckPingMock).toHaveBeenCalledWith('_health');
-  expect(useStub).toHaveBeenCalledWith('foo-bar');
+  expect(robot.on).toHaveBeenCalledWith(expectedListeners, pullRequestHandler);
 });
